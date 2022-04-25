997009dfcdf87e2615c9844d935b4863edc252ab
galileoomega <alexis.limacher@outlook.com>
2022-04-25 11:39:59 +0200

    ajout du formulaire

--- post.php
+++ post.php
@@ -1,3 +1,11 @@
 <?php include "layout/navbar.php"; ?>
-Post works!
+
+<div class="container">
+    <h1>Nouveau post</h1>
+    <form action="">
+        <textarea placeholder="Ecrivez votre message ici..." name="" id="" cols="30" rows="10"></textarea>
+        <input type="file">
+    </form>
+</div>
+
 <?php include "layout/footer.php"; ?>
